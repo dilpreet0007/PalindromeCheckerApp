c39c291cddc8eb1c6f3b2307f52999bad657054b
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2026-03-05 14:13:55 +0530

    UC11 code edited

--- src/com/palindromechecker/UCElevenPalindromeChecker.java
+++ src/com/palindromechecker/UCElevenPalindromeChecker.java
@@ -1,5 +1,30 @@
+
 package com.palindromechecker;
 
+import java.util.Scanner;
+/*
+ * UC11 Used OOPS concept to check string is Palindrome or not
+ * @version 11.0
+ * @developer Dilpreet
+ */
 public class UCElevenPalindromeChecker {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter String: ");
+		String s = sc.nextLine();
+		
+		if(PalService.checkPal(s)) System.out.println("String is Palindrome");
+		else System.out.println("String is not Palindrome");
+	}
+}
 
+class PalService{
+	public static boolean checkPal(String s) {
+		for(int i=0;i<s.length()/2;i++) {
+			if(s.charAt(i)!=s.charAt(s.length()-i-1)) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
