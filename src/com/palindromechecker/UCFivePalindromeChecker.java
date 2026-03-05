f7ce11eb046a37a561549c6eec2a770ec20dd96a
dilpreet0007 <dilpreet.sandhu2004@gmail.com>
2026-03-05 10:28:24 +0530

    UC5 code added

---
+++ src/com/palindromechecker/UCFivePalindromeChecker.java
@@ -0,0 +1,30 @@
+package com.palindromechecker;
+
+import java.util.*;
+
+public class UCFivePalindromeChecker {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter String: ");
+		String s =  sc.nextLine();
+		boolean isPal = true;
+		System.out.println("Input String: " + s);
+		Stack<Character> stack = new Stack<>();
+		
+		for(char c : s.toCharArray()) {
+			stack.push(c);
+		}
+		
+		for(char c : s.toCharArray()) {
+			if(c!=stack.pop()) {
+				isPal = false;
+				break;
+			}
+		}
+		
+		if(isPal) System.out.println("String is Palindrome");
+		else System.out.println("String is Not Palindrome");
+	}
+
+}
